f4cc6a6847c3c0cfc92958b1251a3c32b16bef96
Quazi Ghulam Rafi <quazirafi1@gmail.com>
2024-05-05 14:48:34 +0300

    Class based view/APIView demo

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.blog, name="blog"),
-    path('blog_detail/<int:pk>/', views.blog_detail, name="blog_detail"),
+    # path('blog/', views.blog, name="blog"),
+    # path('blog_detail/<int:pk>/', views.blog_detail, name="blog_detail"),
+
+    #class based view urls
+    path('blog/', views.BlogListView.as_view(), name="blog"),
+    path('blog_detail/<int:pk>', views.BlogDetailView.as_view(), name="blog_detail"),   
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,33 +3,32 @@ from .serializer import BlogSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-# Create your views here.
+from rest_framework.views import APIView
 
-@api_view(['GET', 'POST'])
-def blog(request):
-    if request.method == 'GET':
+#GET, POST
+class BlogListView(APIView):
+    def get(self, request):
         all_blogs = Blog.objects.all()
         serializer = BlogSerializer(all_blogs, many=True)
-        
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    if request.method == 'POST':
+    def post(self, request):
         serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-@api_view(['GET', 'PUT', 'DELETE'])
-def blog_detail(request, pk):
-    if request.method == 'GET':
+
+#GET, PUT, DELETE
+class BlogDetailView(APIView):
+    def get(self, request, pk):
         blog = Blog.objects.get(pk=pk)
         serializer = BlogSerializer(blog)
         
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    if request.method == 'PUT':
+    def put(self, request, pk):
         blog = Blog.objects.get(pk=pk)
         serializer = BlogSerializer(blog, data=request.data)
         
@@ -38,9 +37,48 @@ def blog_detail(request, pk):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    if request.method == 'DELETE':
+        
+    def delete(self, request, pk):
         blog = Blog.objects.get(pk=pk)
         blog.delete()
         return Response(status=status.HTTP_200_OK)
+    
+# @api_view(['GET', 'POST'])
+# def blog(request):
+#     if request.method == 'GET':
+#         all_blogs = Blog.objects.all()
+#         serializer = BlogSerializer(all_blogs, many=True)
+        
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+#     if request.method == 'POST':
+#         serializer = BlogSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def blog_detail(request, pk):
+#     if request.method == 'GET':
+#         blog = Blog.objects.get(pk=pk)
+#         serializer = BlogSerializer(blog)
+        
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+#     if request.method == 'PUT':
+#         blog = Blog.objects.get(pk=pk)
+#         serializer = BlogSerializer(blog, data=request.data)
+        
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#     if request.method == 'DELETE':
+#         blog = Blog.objects.get(pk=pk)
+#         blog.delete()
+#         return Response(status=status.HTTP_200_OK)
         
